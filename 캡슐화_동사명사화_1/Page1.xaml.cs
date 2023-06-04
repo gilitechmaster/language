93d306e8b783a2371e69ca3a87c73f448625bcbd
경섭 <51491577+gilitechmaster@users.noreply.github.com>
2023-06-04 22:54:22 +0900

    Add files via upload

---
+++ 캡슐화_동사명사화_1/Page1.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+using Xamarin.Forms.Xaml;
+
+namespace App11
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        async void OnSaveButtonClicked(object sender, EventArgs e) //저장버튼 누르면
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename)) //문자열이 널인지 공백인지 확인한다.
+            {
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                File.WriteAllText(note.Filename, note.Text);
+            }
+            await Navigation.PopAsync();
+        }
+
+        async void OnCalc1ButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+
+            else
+            {
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(note.Filename, note.Text);
+                StreamWriter sw = new StreamWriter(filename, true);
+                string[] arr = note.Text.Split(new string[]{
+
+                    ". ",  // 순차처리하므로 ". "를 앞에해두어야 한다.
+                    ".",
+                    ", ",
+                    ",",
+                    "? ",
+                    "?",
+                    "! ",
+                    "!",
+                    "·", // 법률문서에 사용되는 기호
+                    " "
+
+                        }, StringSplitOptions.None);
+
+
+                foreach (string B in arr)
+                {
+                    sw.WriteLine(B);
+
+                    if (B.Contains("\n")  // replace로 2차처리
+                        )
+                        sw.WriteLine("{0}", B
+                          .Replace("\n", "")
+                          );
+                }
+
+                sw.Close();
+
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        async void OnCalc2ButtonClicked(object sender, EventArgs e) // 개체분해버튼
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+
+            else
+            {
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(note.Filename, note.Text);
+                StreamWriter sw = new StreamWriter(filename, true);
+                string[] arr = note.Text.Split(new string[]{
+
+                    ". ",  // 순차처리하므로 ". "를 앞에해두어야 한다.
+                    ".",
+                    ", ",
+                    ",",
+                    "? ",
+                    "?",
+                    "! ",
+                    "!",
+                    "·", // 법률문서에 사용되는 기호
+                    " "
+
+                        }, StringSplitOptions.None);
+
+                foreach (string B in arr)
+                {
+
+                    //sw.WriteLine(B);
+
+                    // 개체 + 에너지 = 1최소요소 = NXET
+                    // 개체 = N
+                    // 기호 = X
+                    // 에너지 = E
+                    // 시간 = T
+                    // 1최소요소는 절대요소이다.
+
+                    if (B.EndsWith("사과") == true) // 개체
+                        sw.WriteLine("{0}", B
+                            .Replace("사과", "N")
+                            );
+
+
+                }
+                sw.Close();
+
+            }
+
+            await Navigation.PopAsync();
+        }
+
+       
+        async void OnDeleteButtonClicked(object sender, EventArgs e) // 삭제버튼
+        {
+            var note = (Note)BindingContext;
+            if (File.Exists(note.Filename))
+            {
+                File.Delete(note.Filename);
+            }
+            await Navigation.PopAsync();
+        }
+    }
+}
