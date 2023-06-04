2b33bae07a8063a5eaf92629cfc8a9ae1f2f62ea
경섭 <51491577+gilitechmaster@users.noreply.github.com>
2023-06-04 22:54:53 +0900

    Add files via upload

---
+++ 캡슐화_동사명사화_1/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App11
+{
+    public partial class App : Application
+    {
+        public static string FolderPath { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+            FolderPath = Path.Combine
+                (Environment.GetFolderPath
+                (Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new NavigationPage(new Page2());
+        }
+    }
+}
